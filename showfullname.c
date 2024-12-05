4db21fbc29f13462c26529f01634eb6df37d5df3
yadib <yassedme17@gmai.com>
2024-12-05 09:11:05 +0100

    show last name

--- showfullname.c
+++ showfullname.c
@@ -3,9 +3,12 @@
 int main()
 {
 	char first_name[7];
-	// char last_name[5];
+	char last_name[5];
 
 	printf("enter the first names:\n");
 	scanf("%s", first_name);
+	printf("enter the last names:\n");
+	scanf("%s", last_name);
 	printf("\nfirst name :: %s\n", first_name);
+	printf("\nlast name :: %s\n", last_name);
 }
